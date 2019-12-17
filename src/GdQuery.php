64b30e6016be874f68d5a46dea01cf84a5c7393b
Rafael Brigagao Paulino <rafael.paulino@FIESP.COM.BR>
2019-12-17 12:51:35 -0300

    adicionei as tarefas iniciais

--- src/GdQuery.php
+++ src/GdQuery.php
@@ -4,19 +4,27 @@ namespace GdQuery;
 
 class GdQuery
 {
+    protected $path;
     protected $image;
     protected $width;
     protected $height;
+    protected $type;
 
-    public function __construct($image)
+    public function __construct($path)
     {
-        $this->setImage($image);
-        $info = getimagesize($image);
+        $this->setPath($path);
+        $info = getimagesize($path);
 
-        if (is_array($info) && count($info) > 2 && isset($info[0]) && isset($info[1]) ) {
+        if ( is_array($info) && count($info) > 2 && isset($info[0]) && isset($info[1]) && isset($info[2]) ) {
+            
             $this->setWidth($info[0]);
             $this->setHeight($info[1]);
-        }        
+            $this->setType($info[2]);
+
+            $this->setImage();
+        }
+        
+        return $this;
     }
 
     
@@ -74,9 +82,78 @@ class GdQuery
      *
      * @return  self
      */ 
-    protected function setImage($image)
+    protected function setImage()
     {
+        switch($this->getType())
+        {
+            case 'gif':
+                $image = imagecreatefromgif($this->getPath());
+            break;
+            case 'jpg':
+                $image = imagecreatefromjpeg($this->getPath());
+            break;
+            case 'png':
+                $image = imagecreatefrompng($this->getPath());
+                imagealphablending($image, false);
+                imagesavealpha($image, true);
+            break;
+        }
+
         $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of path
+     */ 
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set the value of path
+     *
+     * @return  self
+     */ 
+    protected function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    protected function setType($type)
+    {
+        switch($type)
+        {
+            case 1:
+                $type = 'gif';
+            break;
+            case 2:
+            default:
+                $type = 'jpg';
+            break;
+            case 3:
+                $type = 'png';
+            break;
+        }
+
+        $this->type = $type;
 
         return $this;
     }
